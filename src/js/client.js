418d8b6e717e043436d45aec18805a1374fc1108
Greg Foreman <gfore@mindspring.com>
2019-03-27 01:03:10 -0400

    adds axis lablss

--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { VictoryBar } from 'victory';
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
 
 const data = [
     {quarter: 1, earnings: 13000},
@@ -19,11 +19,25 @@ class Main extends React.Component {
         return (
             <div>
                 <h1>Victory Tutorial</h1>
-                <VictoryBar
-                    data={data}
-                    x='quarter'
-                    y='earnings'
-                />
+                <VictoryChart domainPadding={8}>
+                    <VictoryAxis
+                        // tickValues specifies both the number of ticks and where
+                        // they are placed on the axis
+                        tickValues={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+                        tickFormat={["Q1", "Q2", "Q3", "Q4", "Q5", "Q6", "Q7", "Q8", "Q9"]}
+                    />
+                    <VictoryAxis
+                        dependentAxis
+                        // tickFormat specifies how ticks should be displayed
+                        tickFormat={(x) => (`$${x / 1000}k`)}
+                    />
+                    <VictoryBar
+                        data={data}
+                        x='quarter'
+                        y='earnings'
+                    />
+                </VictoryChart>
+                <p>Hey!</p>
             </div>
         );
     }
