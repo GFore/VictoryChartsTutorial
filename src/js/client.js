6b20cc2d4d28b71d779c774b8dfc0994b9debb64
Greg Foreman <gfore@mindspring.com>
2019-03-27 00:32:08 -0400

    adds VictoryBar example

--- src/js/client.js
+++ src/js/client.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { VictoryBar } from 'victory';
+
+const data = [
+    {quarter: 1, earnings: 13000},
+    {quarter: 2, earnings: 16500},
+    {quarter: 3, earnings: 14250},
+    {quarter: 4, earnings: 29000},
+    {quarter: 5, earnings: 23000},
+    {quarter: 6, earnings: 26000},
+    {quarter: 7, earnings: 29000},
+    {quarter: 8, earnings: 23000},
+    {quarter: 9, earnings: 26000},
+];
 
 class Main extends React.Component {
     render() {
         return (
             <div>
                 <h1>Victory Tutorial</h1>
-                <p>hi</p>
+                <VictoryBar
+                    data={data}
+                    x='quarter'
+                    y='earnings'
+                />
             </div>
         );
     }
